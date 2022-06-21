3d6976bb40a4943ac8b8bb9ec20cefb6477e7f92
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-06-21 18:34:07 -0300

    Feito o menu do projeto um - Comidas

--- Projetos/Projeto #01 - Comidas/assets/scripts/script.js
+++ Projetos/Projeto #01 - Comidas/assets/scripts/script.js
@@ -1,3 +1,4 @@
+// <Section__1>
 const icons = document.querySelectorAll(".section-1-icons i");
 
 let i = 1;
@@ -18,3 +19,12 @@ setInterval(() => {
     }
     
 }, 1500);
+// </Section__1>
+
+// <NavBar>
+document.querySelector(".menu").addEventListener("click", () => {
+   document.querySelectorAll(".target").forEach((item) => {
+        item.classList.toggle("change")
+   }) 
+})
+// </NavBar>
